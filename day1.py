8fac0c624827cce9f30f033b82adc97606d85278
Pradeep Yadav <javvajipradeep70@gmail.com>
2026-05-06 12:16:55 +0530

    initial commit

---
+++ day1.py
@@ -0,0 +1,86 @@
+# Print Numbers from 1 to n
+# n=int(input("enter the number: "))
+
+
+# for i in range(1,n+1):
+#     print(i)
+
+
+# Print Numbers from m to n
+# m=3
+# n=7
+
+# for i in range(m,n+1):
+#     print(i)
+
+#  Print Numbers from n to 1 in Reverse
+
+
+# n=int(input("enter the numbers: "))
+
+
+# i=1
+
+# while n>=1:
+#     print(n)
+#     n = n-1
+
+# Print Numbers from n to m in Reverse
+
+# n = int(input("Enter n: "))
+# m = int(input("Enter m: "))
+
+# i = n
+# while i >= m:
+#     print(i)
+#     i -= 1
+
+# n=int(input("enter n: "))
+# m=int(input("enter m: "))
+
+# i = n
+
+# while i >= m:
+#     print(i)
+#     i = i-1
+
+# Sum of n Natural Numbers
+
+# n=int(input("enter the number: "))
+
+# i = 1
+# sum=0
+# while i <= n:
+#     sum = sum + i
+#     i += 1
+
+# print(sum)
+
+#  Factorial of a Number
+
+# n=int(input("enter the number: "))
+
+# i = 1
+# fact=1
+# while i <= n:
+#     fact = fact * i
+#     i += 1
+
+# print(fact)
+
+# Sum of m to n Numbers
+
+
+# for loop
+# m=int(input("enter m: "))
+# n=int(input("enter n: "))
+
+
+# sum=0
+# for i in range(m, n+1):
+#     sum = sum + i
+
+# print(sum)
+
+
+
